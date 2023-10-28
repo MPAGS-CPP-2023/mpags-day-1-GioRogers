a6fe7822f9e966f632ae8c39033845a7fa8baaaf
Giovanni Rogers <grr773@student.bham.ac.uk>
2023-10-28 17:59:36 +0000

    Example of simple variable declerations and math operation

---
+++ mpags-cipher.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main()
+{
+    //This is a comment
+    /*This is a multiline
+    comment*/
+
+    //std::cout << "Hello World!\n";
+
+    int a{6};
+    double b{1.2};
+    c = a + b;
+    std::cout << c << std::endl;
+    return c;
+}
